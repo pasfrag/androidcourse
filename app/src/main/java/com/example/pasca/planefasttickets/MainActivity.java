feec1908503ecc7cdee35edd062af376ffabb63e
Paschalis Frangidis <Paschalis Frangidis>
2017-01-18 01:23:01 +0200

    Initial commit

---
+++ app/src/main/java/com/example/pasca/planefasttickets/MainActivity.java
@@ -0,0 +1,215 @@
+package com.example.pasca.planefasttickets;
+
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.ListView;
+
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity  {
+
+    private AutoCompleteTextView originTextView, destinationTextView;
+    private ArrayAdapter<String> airportOriginAdapter, airportDestinationAdapter;
+    private String[] airportNames, airportValues;
+    private int tvController;
+    private ListView originLV, destinationLV;
+    private AirportWatcher originWatcher, destinationWatcher;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        originTextView = (AutoCompleteTextView) findViewById(R.id.atv_origin_location);
+        destinationTextView = (AutoCompleteTextView) findViewById(R.id.atv_destination_location);
+
+        airportNames = null;
+        airportValues = null;
+        tvController = 1;
+
+        originWatcher = new AirportWatcher();
+        destinationWatcher = new AirportWatcher();
+
+        originLV = (ListView) findViewById(R.id.lv_origin);
+        airportOriginAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, new ArrayList<String>());
+        originLV.setAdapter(airportOriginAdapter);
+
+        destinationLV = (ListView) findViewById(R.id.lv_destination);
+        airportDestinationAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, new ArrayList<String>());
+        destinationLV.setAdapter(airportDestinationAdapter);
+
+
+        originTextView.addTextChangedListener(originWatcher);
+        destinationTextView.addTextChangedListener(destinationWatcher);
+    }
+
+
+    //This class gets the airport names and values from amadeus api
+    //and fills the adapters
+    class FetchLocationTask extends AsyncTask<String, Void, String>{
+
+        public FetchLocationTask(){
+
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            if (params.length==0){
+                return null;
+            }
+            String location = params[0];
+
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+            String airportJSONStr = null;
+
+            try {
+                Uri builtUri = UrlBuildingUtils.locationUri(location);
+                URL url = new URL(builtUri.toString());
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream stream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+
+                if (stream == null){
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length()==0){
+                    return null;
+                }
+
+                airportJSONStr = buffer.toString();
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            }   catch (MalformedURLException e) {
+              e.printStackTrace();
+            }   catch (IOException e) {
+              e.printStackTrace();
+            }   catch (NullPointerException e) {
+              e.printStackTrace();
+            }
+
+
+            return airportJSONStr;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            getJSONData(s);
+
+            if (tvController == 1){
+                adjustTheAdapters(airportOriginAdapter, originLV);
+            }else if (tvController == 2){
+                adjustTheAdapters(airportDestinationAdapter, destinationLV);
+            }
+
+        }
+
+
+    }
+
+    //This class watches for changes in the text views
+    class AirportWatcher implements TextWatcher {
+
+
+
+        public AirportWatcher(){
+
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(final Editable s) {
+
+
+            if (s.length()<3){
+                return;
+            }
+            View view = getCurrentFocus();
+            if (view == originTextView){
+                tvController = 1;
+            }
+            else if(view == destinationTextView) {
+                tvController = 2;
+            }
+
+            new FetchLocationTask().execute(s.toString());
+
+        }
+    }
+
+    public void getJSONData(String JSONString) {
+
+        try {
+            JSONArray airportsArray = new JSONArray(JSONString);
+
+            airportNames = new String[airportsArray.length()];
+            airportValues = new String[airportsArray.length()];
+
+            for (int i=0; i<airportsArray.length();i++){
+                JSONObject object = airportsArray.getJSONObject(i);
+                airportNames[i] = object.getString("label");
+                airportValues[i] = object.getString("value");
+            }
+
+        } catch (JSONException e) {
+          e.printStackTrace();
+        }
+
+    }
+
+    public void adjustTheAdapters(ArrayAdapter<String> adapter, ListView listView){
+        if(adapter != null) {
+            adapter.clear();
+        }
+
+        for (String airportName : airportNames) {
+            adapter.add(airportName);
+            adapter.notifyDataSetChanged();
+        }
+
+        listView.setVisibility(View.VISIBLE);
+    }
+}
---
+++ app/src/main/java/com/example/pasca/planefasttickets/UrlBuildingUtils.java
@@ -0,0 +1,53 @@
+package com.example.pasca.planefasttickets;
+
+
+import android.net.Uri;
+
+
+public class UrlBuildingUtils {
+    private final static String BASE_URL_AUTOCOMPLETE = "https://api.sandbox.amadeus.com/v1.2/airports/autocomplete?";
+    private final static String BASE_URL = "https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?";
+    private final static String API_KEY_PARAM = "apikey";
+    private final static String TERM = "term";
+    private final static String ORIGIN_PARAM = "origin";
+    private final static String DESTINATION_PARAM = "destination";
+    private final static String DEPARTURE_PARAM = "departure_date";
+    private final static String RETURN_PARAM = "return_date";
+    private final static String ADULTS_PARAM = "adults";
+    private final static String CHILDREN_PARAM = "children";
+    private final static String STOP_PARAM = "non_stop";
+    private final static String PRICE_PARAM = "max_price";
+    private final static String CURRENCY_PARAM = "currency";
+    private final static String RESULTS = "number_of_results";
+    private final static String API_KEY = "tIHCa5bMukDGHL8h0439NVyLuVEwxwMx";
+
+
+    //Builds the uri
+    public static Uri locationUri(String term){
+        Uri builtUri = Uri.parse(BASE_URL_AUTOCOMPLETE).buildUpon()
+            .appendQueryParameter(API_KEY_PARAM,API_KEY)
+            .appendQueryParameter(TERM,term)
+            .build();
+
+        return builtUri;
+    }
+
+    public Uri flightSearchUri(String origin, String destination, String depDate, String retDate, String adults, String children, String stops, String price, String currency){
+        Uri builtUri = Uri.parse(BASE_URL).buildUpon()
+            .appendQueryParameter(API_KEY_PARAM,API_KEY)
+            .appendQueryParameter(ORIGIN_PARAM, origin)
+            .appendQueryParameter(DESTINATION_PARAM, destination)
+            .appendQueryParameter(DEPARTURE_PARAM, depDate)
+            .appendQueryParameter(RETURN_PARAM, retDate)
+            .appendQueryParameter(ADULTS_PARAM, adults)
+            .appendQueryParameter(CHILDREN_PARAM, children)
+            .appendQueryParameter(STOP_PARAM, stops)
+            .appendQueryParameter(PRICE_PARAM, price)
+            .appendQueryParameter(CURRENCY_PARAM, currency)
+            .appendQueryParameter(RESULTS, "10")
+            .build();
+
+        return builtUri;
+    }
+
+}
